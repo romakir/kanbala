97e14bc0205e0cd6e25d02a785058f5c0f1641dd
Ivan V. Durnev <durnev87@gmail.com>
2020-10-03 03:24:24 +0900

    верстка главной

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,3 +1,4 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, DateField, TimeField, FloatField, SelectField, IntegerField, TextAreaField
 from wtforms.validators import DataRequired
+
