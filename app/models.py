9cf7717adb7f6df69142383322e0a54c0417f879
Ivan V. Durnev <durnev87@gmail.com>
2020-10-02 19:09:14 +0900

    обновление структуры БД

--- app/models.py
+++ app/models.py
@@ -19,6 +19,7 @@ class User(UserMixin, db.Model):
     position = db.Column(db.String(256))
     email = db.Column(db.String(256))
     password_hash = db.Column(db.String(128))
+    userpic = db.Column(db.Binary)
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
@@ -45,3 +46,28 @@ class User(UserMixin, db.Model):
 
     def __repr__(self):
         return f'{self.first_name} {self.last_name}'
+
+
+class Regulation(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    short_name = db.Column(db.String(512))
+    description = db.Column(db.String(2048))
+
+
+class RegulationVersion(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    regulation_id = db.Column(db.Integer, db.ForeignKey('regulation.id'))
+    version_number = db.Column(db.Integer)
+    status = db.Column(db.String(20))
+    data = db.Column(db.JSON)
+
+    def parent_regulation(self):
+        return Regulation.query.get(self.regulation_id)
+
+
+class UserRegulation(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    regulation_version_id = db.Column(db.Integer, db.ForeignKey('regulation_version.id'))
+    mode = db.Column(db.String(20))
+    comments_data = db.Column(db.JSON)
